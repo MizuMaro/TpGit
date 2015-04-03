d862ade5519cefe1b4d2685228e32279dd39ddba
MizuMaro <lil_hasni@hotmail.fr>
2015-04-03 16:27:38 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,13 +1,71 @@
 package iut.tdd;
 
+import java.util.HashMap;
+
 public class Convert {
-	public static String num2text(String input) {
-            if(input == "0")
-                return "zéro";
-            else 
-                return null; 
-	}
-	public static String text2num(String input) {
-		return null;
-	}
+
+    public static final String ZERO = "zéro";
+    public static final String UN = "un";
+    public static final String DEUX = "deux";
+    public static final String TROIS = "trois";
+    public static final String QUATRE = "quatre";
+    public static final String CINQ = "cinq";
+    public static final String SIX = "six";
+    public static final String SEPT = "sept";
+    public static final String HUIT = "huit";
+    public static final String NEUF = "neuf";
+    public static final String DIX = "dix";
+    public static final String ONZE = "onze";
+    public static final String DOUZE = "douze";
+    public static final String TREIZE = "treize";
+    public static final String QUATORZE = "quatorze";
+    public static final String QUINZE = "quinze";
+    public static final String SEIZE = "seize";
+    public static final String VINGT = "vingt";
+    public static final String TRENTE = "trente";
+    public static final String QUARANTE = "quarante";
+    public static final String CINQUANTE = "cinquante";
+    public static final String SOIXANTE = "soixante";
+
+    public static final String CENT = "cent";
+    public static final String CENTS = "cents";
+
+    public static String num2text(String input) {
+
+        HashMap<String, String> map = new HashMap();
+
+        map.put("0", ZERO);
+        map.put("1", UN);
+        map.put("2", DEUX);
+        map.put("3", TROIS);
+        map.put("4", QUATRE);
+        map.put("5", CINQ);
+        map.put("6", SIX);
+        map.put("7", SEPT);
+        map.put("8", HUIT);
+        map.put("9", NEUF);
+        map.put("10", DIX);
+        map.put("11", ONZE);
+        map.put("12", DOUZE);
+        map.put("13", TREIZE);
+        map.put("14", QUATORZE);
+        map.put("15", QUINZE);
+        map.put("16", SEIZE);
+        map.put("20", VINGT);
+        map.put("30", TRENTE);
+        map.put("40", QUARANTE);
+        map.put("50", CINQUANTE);
+        map.put("60", SOIXANTE);
+
+        return map.get(input);
+    }
+
+    public int nbChiffre(String input) {
+        return input.length();
+
+    }
+
+    public static String text2num(String input) {
+        return null;
+    }
 }
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -5,16 +5,131 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class TestConvert {
-	@Test
-	public void test_num2text_zero () {
+
+    @Test
+    public void test_num2text_zero() {
+
+        Assert.assertEquals("zéro", Convert.num2text("0"));
+    }
+
+    @Test
+    public void test_num2text_un() {
+        Assert.assertEquals("un","1");
+    }
+
+    @Test
+    public void test_num2text_deux() {
+        Assert.assertEquals("deux", Convert.num2text("2"));
+    }
+
+    @Test
+    public void test_num2text_trois() {
+        Assert.assertEquals("trois", Convert.num2text("3"));
+    }
+
+    @Test
+    public void test_num2text_quatre() {
+        // Given
+        String input = "4";
+        String expected = "quatre";
+        Convert myConvert = new Convert();
+        // When
+        String actual = myConvert.num2text(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_num2text_cinq() {
+        // Given
+        String input = "5";
+        String expected = "cinq";
+        Convert myConvert = new Convert();
+        // When
+        String actual = myConvert.num2text(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_num2text_six() {
+        // Given
+        String input = "6";
+        String expected = "six";
+        Convert myConvert = new Convert();
+        // When
+        String actual = myConvert.num2text(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_num2text_sept() {
+        // Given
+        String input = "7";
+        String expected = "sept";
+        Convert myConvert = new Convert();
+        // When
+        String actual = myConvert.num2text(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    	public void test_num2text_huit () {
 		// Given
-		String input = "0";
-		String expected = "zéro";
+		String input = "8";
+		String expected = "huit";
 		Convert myConvert = new Convert();
 		// When
 		String actual = myConvert.num2text(input);
 		// Then
 		Assert.assertEquals(expected, actual);
 	}
-	
+    @Test
+        	public void test_num2text_neuf () {
+		// Given
+		String input = "9";
+		String expected = "neuf";
+		Convert myConvert = new Convert();
+		// When
+		String actual = myConvert.num2text(input);
+		// Then
+		Assert.assertEquals(expected, actual);
+	}
+    @Test
+                	public void test_num2text_ () {
+		// Given
+		String input = "10";
+		String expected = "dix";
+		Convert myConvert = new Convert();
+		// When
+		String actual = myConvert.num2text(input);
+		// Then
+		Assert.assertEquals(expected, actual);
+	}
+    @Test
+                	public void test_num2text_onze () {
+		// Given
+		String input = "11";
+		String expected = "onze";
+		Convert myConvert = new Convert();
+		// When
+		String actual = myConvert.num2text(input);
+		// Then
+		Assert.assertEquals(expected, actual);
+	}
+    @Test
+                	public void test_num2text_douze () {
+		// Given
+		String input = "12";
+		String expected = "douze";
+		Convert myConvert = new Convert();
+		// When
+		String actual = myConvert.num2text(input);
+		// Then
+		Assert.assertEquals(expected, actual);
+	}
+
+
+
 }
